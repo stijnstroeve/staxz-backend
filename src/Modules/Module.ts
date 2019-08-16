67b0e719aa6291a03307ba2114091dd2a919c85f
Stijn Stroeve <stijnstroeve@hotmail.com>
2019-08-16 18:59:49 +0200

    Removed commented code.

--- src/Modules/Module.ts
+++ src/Modules/Module.ts
@@ -4,24 +4,9 @@ import {FallbackModule} from "./FallbackModule";
 
 export abstract class Module {
 
-    // static modules: Module[];
-
     abstract name: string;
     abstract moduleMethods: ModuleMethod[];
 
     abstract registerModuleMethods(): void;
-
-    // static registerModules() {
-    //     let userModule: Module = new UserModule();
-    //
-    //     userModule.registerModuleRequests();
-    //
-    //     this.modules.push(userModule);
-    // }
-    //
-    // static getModule(name: string): Module {
-    //     let module = this.modules.find((module) => module.name === name);
-    //     return module ? module : new FallbackModule();
-    // }
 
 }
